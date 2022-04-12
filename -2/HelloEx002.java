40f48195d562e15078c5339f436a1f1b08aca95e
이준석 <iamjun1128@naver.com>
2022-04-12 02:20:56 +0000

    22

---
+++ -2/HelloEx002.java
@@ -0,0 +1,7 @@
+public class HelloEx002 {
+    public static void main(String[]args){
+        System.out.print(100);
+        System.out.println(100);
+        System.out.println("합격점수 : " +100);
+    }
+}
